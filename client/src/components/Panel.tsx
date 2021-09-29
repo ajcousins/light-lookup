53e035a9db5dd2f0aa8b92b703b6535866896dc1
Alvin Cousins <alvin.cousins@googlemail.com>
2021-09-29 14:36:47 +0100

    Panel colour

--- client/src/components/Panel.tsx
+++ client/src/components/Panel.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Paper from "@mui/material/Paper";
 
 interface IProps {
   title: string;
@@ -10,18 +9,8 @@ const Panel: React.FC<IProps> = ({ title, className, children }) => {
   const classString = `panel panel__${className}`;
   return (
     <div className={classString}>
-      <Paper
-        sx={{
-          height: "100%",
-          width: "100%",
-          padding: "0.5em",
-          backgroundColor: "#7c7c7c",
-        }}
-        elevation={5}
-      >
-        <h4>{title}</h4>
-        {children}
-      </Paper>
+      <h4>{title}</h4>
+      {children}
     </div>
   );
 };
